f2ab12592aae88e26172a167ce7dfd95cbb66da3
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-02-24 10:51:33 +1300

    Add files via upload

---
+++ src/js/sw.js
@@ -0,0 +1,44 @@
+const cacheName = 'cumulative-time-limit-cache-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/full.js',
+];
+
+// Install event - caching assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(assets);
+        })
+    );
+});
+
+// Activate event - cleaning up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [cacheName];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (!cacheWhitelist.includes(cache)) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event - serving assets from cache when offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })
+    );
+});
